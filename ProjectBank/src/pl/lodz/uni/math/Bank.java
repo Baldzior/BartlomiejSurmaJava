2b819439586fb53ff263c27ae95434111fa25ceb
Bartek <Bartek@Bartek-Komputer>
2017-03-08 16:52:14 +0100

    commit 2

---
+++ ProjectBank/src/pl/lodz/uni/math/Bank.java
@@ -0,0 +1,8 @@
+package pl.lodz.uni.math;
+
+public class Bank {
+	private String name;
+	private String city;
+	private String street;
+	private int id;
+}
---
+++ ProjectBank/src/pl/lodz/uni/math/Main.java
@@ -0,0 +1,10 @@
+package pl.lodz.uni.math;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
