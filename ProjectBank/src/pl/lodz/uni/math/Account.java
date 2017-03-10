f3a55cd6fe83da292d95aa93ff276107a82b7aed
Bartek <Bartek@Bartek-Komputer>
2017-03-10 21:44:42 +0100

    10032017

--- ProjectBank/src/pl/lodz/uni/math/Account.java
+++ ProjectBank/src/pl/lodz/uni/math/Account.java
@@ -5,6 +5,7 @@ public class Account {
    private String accountId;
    private Double balance;
    private Transaction[] transactions = new Transaction[10];
-}
+
 
 public void makeTransaction(TransactionType type, double value){}
+}
