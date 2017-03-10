f6dbfed7c7ef8b6f16b5d0a91bc8ab879ff3fd86
Bartek <Bartek@Bartek-Komputer>
2017-03-10 21:41:19 +0100

    commit

--- ProjectBank/src/pl/lodz/uni/math/Account.java
+++ ProjectBank/src/pl/lodz/uni/math/Account.java
@@ -1,10 +1,10 @@
 package pl.lodz.uni.math;
 
 public class Account {
-	private Client owner;
-	private String accountId;
-	private Double balance;
-	private Transaction[] transactions = new Transaction[10];
+   private Client owner;
+   private String accountId;
+   private Double balance;
+   private Transaction[] transactions = new Transaction[10];
 }
 
 public void makeTransaction(TransactionType type, double value){}
